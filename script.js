cc919758e750fc3b1b8b6fce0ce405ce286e9ce7
Oksana Walters <115512168+oksanawalters@users.noreply.github.com>
2023-01-09 21:27:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
+// Profile Picture
 
-// Profile image & video
 let profileImg = document.querySelector(".profile-img");
 let video = document.querySelector(".profile-video");
 
@@ -8,23 +8,6 @@ profileImg.addEventListener('mouseover', () => {
     video.style.display = "block";
     profileImg.style.display = "none";
 });
-
-// let videoEnded = false;
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-//    let myPromise = new Promise(()=>{
-//     player.on('ended', function(){
-//         videoEnded = true;
-//     });
-//    })
-
-
-// myPromise.then(()=>{
-//     video.addEventListener('mouseout', () => {
-//     video.style.display = "none";
-//     profileImg.style.display = "block";})
-// });
 
 video.addEventListener('mouseout', () => {
         video.style.display = "none";
